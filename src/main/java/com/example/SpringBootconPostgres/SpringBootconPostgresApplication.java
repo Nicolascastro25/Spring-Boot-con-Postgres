3114351f8f93e2884273845f6c560c2f306f3fe8
Jaime Nicolás Castro Acuña <jaime.nicolas.castro@gmail.com>
2023-02-27 11:41:22 -0500

    lab finalizado

---
+++ src/main/java/com/example/SpringBootconPostgres/SpringBootconPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootconPostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootconPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootconPostgresApplication.class, args);
+	}
+
+}
